f64f3ad5a8f1b0dff485977279f7562091d767a0
Ponnusamy K <ponnusamy@francium.tech>
2018-07-15 14:34:40 +0530

    Assignment component

---
+++ assignment/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <header-comp></header-comp>
+        <hr>
+        <server-status></server-status>
+        <hr>
+        <footer-comp></footer-comp>
+    </div>
+</template>
+
+<script>
+    import ServerStatus from './ServerStatus.vue'
+    export default {
+        components: {
+            'server-status': ServerStatus 
+        }
+    }
+</script>
+
+<style>
+</style>
