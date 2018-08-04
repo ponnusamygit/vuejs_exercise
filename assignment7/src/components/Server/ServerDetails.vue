7315af83562cf5e7779eb84c839c092005fd8a7d
Ponnusamy K <ponnusamy@francium.tech>
2018-08-04 17:58:33 +0530

    Assignment 7 from component communication bus

---
+++ assignment7/src/components/Server/ServerDetails.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="col-xs-12 col-sm-6" v-if='!server.id'>
+    <p>Server Details are currently not updated.</p>
+    <p>
+      <= Select a server from this list.
+    </p>
+  </div>
+  <div class="col-xs-12 col-sm-6" v-else>
+    <p>Server id # {{ server.id }}</p>
+    <p :class='server.status'>Server status # {{ server.status }}</p>
+  </div>
+</template>
+
+<script>
+  import {eventBus} from '../../main';
+  export default {
+    data: function(){
+      return {
+        server: {}
+      }
+    },
+    created(){
+      eventBus.$on('ServerWasChanged', (server)=>{
+        this.server = server;
+      })
+    }
+  }
+</script>
+<style>
+  .Normal{
+    background: green;
+  }
+  .Cretical{
+    background: red;
+  }
+  .Unknown{
+    background: yellow;
+  }
+</style>
---
+++ assignment7/src/components/Server/Servers.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <ul class="list-group">
+            <li
+                    class="list-group-item"
+                    v-for="server in servers"
+                    @click='showAlert(server)'
+                    >
+
+                Server #{{ server.id }} - {{ server.status }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import {eventBus} from '../../main';
+
+    export default {
+      data: function() {
+        return {
+          servers: [
+            { id: 1, status: 'Normal'},
+            { id: 2, status: 'Cretical'},
+            { id: 3, status: 'Unknown'},
+            { id: 4, status: 'Normal'}
+          ]
+        }
+      },
+      methods: {
+        showAlert(server){
+          eventBus.changeServer(server)
+        }
+      }
+    }
+</script>
+
+<style>
+
+</style>
